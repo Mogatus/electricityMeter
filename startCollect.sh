2ada20ae142e73035e06c69f629759a3b0c39010
Jojo <johannes@krimm.name>
2025-09-27 08:19:09 +0200

    shell scripts for collecting data and starting flask server

--- startCollect.sh
+++ startCollect.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 source /home/jojo/devel/electricityMeter/my_venv/bin/activate
+<<<<<<< HEAD
 python3 /home/jojo/devel/electricityMeter/main.py
+=======
+python3 /home/jojo/devel/electricityMeter/main.py
+>>>>>>> c0302c1 (shell scripts for collecting data and starting flask server)
--- startServer.sh
+++ startServer.sh
@@ -3,4 +3,8 @@
 source /home/jojo/devel/electricityMeter/my_venv/bin/activate
 export FLASK_APP=electricityServer
 export FLASK_ENV=development
+<<<<<<< HEAD
 flask run --host=0.0.0.0
+=======
+flask run --host=0.0.0.0
+>>>>>>> c0302c1 (shell scripts for collecting data and starting flask server)
