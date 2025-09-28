123cc931b9ee3a28c19b5b316721afe42fdaa8b8
Jojo <johannes@krimm.name>
2025-09-28 08:18:41 +0200

    mode changed

--- startServer.sh
+++ startServer.sh
@@ -3,8 +3,4 @@
 source /home/jojo/devel/electricityMeter/my_venv/bin/activate
 export FLASK_APP=electricityServer
 export FLASK_ENV=development
-<<<<<<< HEAD
-flask run --host=0.0.0.0
-=======
-flask run --host=0.0.0.0
->>>>>>> c0302c1 (shell scripts for collecting data and starting flask server)
+screen flask run --host=0.0.0.0 
