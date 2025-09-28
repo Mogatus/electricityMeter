6907bb9462b5770f6920717e99112a1eb709955d
Jojo <johannes@krimm.name>
2025-09-28 08:39:45 +0200

    conflict resolved

--- startCollect.sh
+++ startCollect.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 source /home/jojo/devel/electricityMeter/my_venv/bin/activate
-<<<<<<< HEAD
 python3 /home/jojo/devel/electricityMeter/main.py
-=======
-python3 /home/jojo/devel/electricityMeter/main.py
->>>>>>> c0302c1 (shell scripts for collecting data and starting flask server)
